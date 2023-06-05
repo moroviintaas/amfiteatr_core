c4e3e6546cece02f3c3358f0f6e96105b6359c00
moroviintaas <moroviintaas@gmail.com>
2023-06-05 21:09:04 +0200

    Added Send requirement for Reward

--- src/reward.rs
+++ src/reward.rs
@@ -1,10 +1,10 @@
 use std::fmt::Debug;
 use std::ops::{Add, Sub};
 
-pub trait Reward: Clone + Debug + PartialEq + Eq + PartialOrd + Default + Add<Output=Self> + Sub<Output=Self>
+pub trait Reward: Send + Clone + Debug + PartialEq + Eq + PartialOrd + Default + Add<Output=Self> + Sub<Output=Self>
 {
 //where for<'a> &'a Self: Add<Output=Self> + Sub<Output=Self>{
 
 }
 
-impl<T: Clone + Debug + PartialEq + Eq + PartialOrd + Default + Add<Output=Self> + Sub<Output=Self>> Reward for T {}
+impl<T: Send + Clone + Debug + PartialEq + Eq + PartialOrd + Default + Add<Output=Self> + Sub<Output=Self>> Reward for T {}
