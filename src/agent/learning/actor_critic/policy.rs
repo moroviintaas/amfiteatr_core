93aa33b4df01b5abc9a32fc8ea2ac55aa5bfdab7
moroviintaas <moroviintaas@gmail.com>
2023-07-03 21:59:22 +0200

    Implemented Policy for a2c. To be tested

--- src/agent/learning/actor_critic/policy.rs
+++ src/agent/learning/actor_critic/policy.rs
@@ -1,33 +1,70 @@
+use std::fmt::Debug;
 use std::marker::PhantomData;
+use tch::Kind::Float;
 use tch::nn::Optimizer;
 use crate::{InformationSet, Policy};
-use crate::learning::{SelfExperiencingPolicy, GameTrace, NeuralNet2, TensorBuilder};
+use crate::learning::{SelfExperiencingPolicy, GameTrace, NeuralNet2, TensorBuilder, TensorInterpreter};
 use crate::protocol::DomainParameters;
 
 
 
-pub struct ActorCriticPolicy<DP: DomainParameters, InfoSet: InformationSet<DP>, TB: TensorBuilder<InfoSet>>{
+pub struct ActorCriticPolicy<
+    DP: DomainParameters,
+    InfoSet: InformationSet<DP> + Debug,
+    StateConverter: TensorBuilder<InfoSet>,
+    ActInterpreter: TensorInterpreter<Option<DP::ActionType>>
+> {
     network: NeuralNet2,
     optimizer: Optimizer,
     _dp: PhantomData<DP>,
     _is: PhantomData<InfoSet>,
-    state_converter: TB,
+    state_converter: StateConverter,
+    action_interpreter: ActInterpreter
 
 }
-/*
-impl<DP: DomainParameters, InfoSet: InformationSet<DP>, TB: TensorBuilder<InfoSet>> Policy<DP> for ActorCriticPolicy<DP, InfoSet, TB>
-where DP::ActionType: From<i64>{
+
+impl<
+    DP: DomainParameters,
+    InfoSet: InformationSet<DP> + Debug,
+    StateConverter: TensorBuilder<InfoSet>,
+    ActInterpreter: TensorInterpreter<Option<DP::ActionType>>
+> ActorCriticPolicy<DP, InfoSet, StateConverter, ActInterpreter>{
+    pub fn new(network: NeuralNet2,
+               optimizer: Optimizer,
+               state_converter: StateConverter,
+               action_interpreter: ActInterpreter) -> Self{
+        Self{network, optimizer, state_converter, action_interpreter, _dp: Default::default(), _is: Default::default()}
+    }
+}
+
+impl<DP: DomainParameters,
+    InfoSet: InformationSet<DP> + Debug,
+    TB: TensorBuilder<InfoSet>,
+    ActInterpreter: TensorInterpreter<Option<DP::ActionType>>
+> Policy<DP> for ActorCriticPolicy<DP, InfoSet, TB, ActInterpreter>{
     type StateType = InfoSet;
 
     fn select_action(&self, state: &Self::StateType) -> Option<DP::ActionType> {
-        let state_tensor = self.state_converter.build_tensor(state);
-        let (critic, actor) = tch::no_grad(|| (self.network.net())(&state_tensor));
+        let state_tensor = self.state_converter.build_tensor(state)
+            .expect(&format!("Failed converting state to Tensor: {:?}", state));
+        let (_critic, actor) = tch::no_grad(|| (self.network.net())(&state_tensor));
+
+        //somewhen it may be changed with temperature
+        let probs = actor.softmax(-1, Float);
+        let atensor = probs.multinomial(1, true);
+        let action_opt = self.action_interpreter.interpret_tensor(&atensor)
+            .expect("Failed converting tensor to action");
+        action_opt
 
     }
 }
 
- */
-impl<DP: DomainParameters, InfoSet: InformationSet<DP>, TB: TensorBuilder<InfoSet>> SelfExperiencingPolicy<DP> for ActorCriticPolicy<DP, InfoSet, TB>
+
+impl<
+    DP: DomainParameters,
+    InfoSet: InformationSet<DP> + Debug,
+    TB: TensorBuilder<InfoSet>,
+    ActInterpreter: TensorInterpreter<Option<DP::ActionType>>> SelfExperiencingPolicy<DP> for ActorCriticPolicy<DP, InfoSet, TB, ActInterpreter>
 where DP::ActionType: From<i64>{
     type PolicyUpdateError = tch::TchError;
 
--- src/agent/learning/tensor_repr.rs
+++ src/agent/learning/tensor_repr.rs
@@ -1,6 +1,11 @@
 use tch::Tensor;
 
 pub trait TensorBuilder<T>{
-    fn build_tensor(&self, t: &T) -> Tensor;
+    type Error: std::error::Error;
+    fn build_tensor(&self, t: &T) -> Result<Tensor, Self::Error>;
 }
 
+pub trait TensorInterpreter<T>{
+    type Error: std::error::Error;
+    fn interpret_tensor(&self, tensor: &Tensor) -> Result<T, Self::Error>;
+}
