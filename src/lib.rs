c625866a396df3449bb022c174affc3f85814da5
moroviintaas <moroviintaas@gmail.com>
2023-07-07 18:28:32 +0200

    Changed implementation of reward for std types

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-//! author: moroviintaas  
+//! # Sztorm
+//!
 //! Licence: MIT
+//!
+//! The objective of this crate is to provide framework for simulating interactions
+//! between players. Players may follow defined policies or my be run in reinforcement
+//! learning sessions to develop their policies.
 pub mod state;
 pub mod agent;
 mod action;
--- src/reward.rs
+++ src/reward.rs
@@ -1,7 +1,7 @@
 use std::fmt::Debug;
 use std::ops::{Add, AddAssign, Sub};
 
-pub trait Reward: Send + Clone + Debug + PartialEq + Eq + PartialOrd + Default +
+pub trait Reward: Send + Clone + Debug + PartialEq  + PartialOrd + Default +
     for<'a> Add<&'a Self, Output=Self> + Add<Output=Self> + for<'a> AddAssign<&'a Self>
     + Sub<Output=Self> + for<'a> Sub<&'a Self, Output=Self>
 {
@@ -10,12 +10,39 @@ pub trait Reward: Send + Clone + Debug + PartialEq + Eq + PartialOrd + Default +
 //where for<'a> &'a Self: Add<Output=Self> + Sub<Output=Self>{
 
 }
-
+/*
 impl<T: Send + Clone + Debug + PartialEq + Eq + PartialOrd + Default +
     for<'a> Add<&'a Self, Output=Self> + Add<Output=Self>  + for<'a> AddAssign<&'a Self>
     + Sub<Output=Self> + for<'a> Sub<&'a Self, Output=Self>
     + Default> Reward for T {
     fn neutral() -> Self {
         T::default()
+    }
+}*/
+
+macro_rules! impl_reward_std {
+    ($($x: ty), +) => {
+        $(
+          impl Reward for $x{
+              fn neutral() -> $x{
+                  0
+              }
+          }
+
+        )*
+
+    }
+}
+
+impl_reward_std![u8, u16, u32, u64, i8, i16, i32, i64];
+
+impl Reward for f32{
+    fn neutral() -> Self {
+        0.0
+    }
+}
+impl Reward for f64{
+    fn neutral() -> Self {
+        0.0
     }
 }
