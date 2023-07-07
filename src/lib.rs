057bb6501ea242113041e95bad0649097eca56ea
moroviintaas <moroviintaas@gmail.com>
2023-07-07 18:48:39 +0200

    Started documenting front page of the crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
 //! # Sztorm
 //!
-//! Licence: MIT
+//! __Licence:__ MIT
 //!
 //! The objective of this crate is to provide framework for simulating interactions
 //! between players. Players may follow defined policies or my be run in reinforcement
 //! learning sessions to develop their policies.
+
+
+/// Traits for managing state (or information set) of the game.
 pub mod state;
+/// Traits and generic implementations of agent (player).
 pub mod agent;
 mod action;
+/// Generic structs used in communication between _agents_ and _environment_.
 pub mod protocol;
+/// Traits and basic implementation for communication driving structs.
 pub mod comm;
+/// Structures used for error handling in framework.
 pub mod error;
+/// Traits and generic implementations for game controlling environment.
 pub mod env;
 mod reward;
 
--- src/reward.rs
+++ src/reward.rs
@@ -1,11 +1,16 @@
 use std::fmt::Debug;
 use std::ops::{Add, AddAssign, Sub};
 
+/// `Reward` is trait for types to be used as policy performance assessment.
+/// It is implemented for standard types and you can use your own type as reward,
+/// if only you made it partially comparable and summable.
 pub trait Reward: Send + Clone + Debug + PartialEq  + PartialOrd + Default +
     for<'a> Add<&'a Self, Output=Self> + Add<Output=Self> + for<'a> AddAssign<&'a Self>
     + Sub<Output=Self> + for<'a> Sub<&'a Self, Output=Self>
 {
-
+    /// This is constructor used to produce neutral value of reward, i.e.
+    /// the reward that does not change the score. For standard numeric
+    /// types this is just value of 0.
     fn neutral() -> Self;
 //where for<'a> &'a Self: Add<Output=Self> + Sub<Output=Self>{
 
