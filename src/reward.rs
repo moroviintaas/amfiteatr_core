765ce9af4be2ff36360221ad97968ed4b1a1dc9d
moroviintaas <moroviintaas@gmail.com>
2023-09-05 20:13:15 +0200

    Added experimental ProportionalRewaed
    probably will be deleted

--- src/reward.rs
+++ src/reward.rs
@@ -1,19 +1,30 @@
 use std::fmt::Debug;
-use std::ops::{Add, AddAssign, Sub};
+use std::ops::{Add, AddAssign, Div, Sub};
+
 
 /// `Reward` is trait for types to be used as policy performance assessment.
 /// It is implemented for standard types and you can use your own type as reward,
 /// if only you made it partially comparable and summable.
 pub trait Reward: Send + Clone + Debug + PartialEq  + PartialOrd + Default +
     for<'a> Add<&'a Self, Output=Self> + Add<Output=Self> + for<'a> AddAssign<&'a Self>
-    + Sub<Output=Self> + for<'a> Sub<&'a Self, Output=Self> + Sub + Add
-{
+    + Sub<Output=Self> + for<'a> Sub<&'a Self, Output=Self> + Sub + Add {
     /// This is constructor used to produce neutral value of reward, i.e.
     /// the reward that does not change the score. For standard numeric
     /// types this is just value of 0.
     fn neutral() -> Self;
 //where for<'a> &'a Self: Add<Output=Self> + Sub<Output=Self>{
 //where for<'a> &'a Self: Sub<&'a Self, Output=Self>
+}
+
+/*
+pub trait ProportionalReward<Float>: Reward
+where for<'a>& 'a Self: Add<&'a Self, Output=Self>,
+for<'a> &'a Self: Div<&'a Self, Output=Float>{}
+
+ */
+
+pub trait ProportionalReward<Float>: Reward{
+    fn proportion(&self, other: &Self) -> Float;
 }
 /*
 impl<T: Send + Clone + Debug + PartialEq + Eq + PartialOrd + Default +
@@ -56,4 +67,22 @@ impl Reward for f64{
 pub enum RewardSource{
     Env,
     Agent
+}
+
+/*
+impl ProportionalReward<f32> for f32{}
+impl ProportionalReward<f64> for f64{}
+impl ProportionalReward<f32> for i64{}
+ */
+
+impl ProportionalReward<f32> for f32{
+    fn proportion(&self, other: &Self) -> f32 {
+        self / other
+    }
+}
+
+impl ProportionalReward<f32> for i32{
+    fn proportion(&self, other: &Self) -> f32 {
+        *self as f32/ *other as f32
+    }
 }
